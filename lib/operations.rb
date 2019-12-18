7d1708caf4448eb70f9a5a75a121e356469ede5d
Eric murray <ericwmurray1994@gmail.com>
2019-12-18 04:54:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,9 +2,9 @@
 
 def unsafe?(speed)
   if speed > 60
-    return "true"
+    return true
   elsif speed < 40
-    return "true"
+    return true
   else speed > 40 && speed < 60
     return "true"
   end
